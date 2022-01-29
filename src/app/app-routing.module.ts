03562be4569df10f30f468ea939aeda79694bffb
Henrry Mojica <hleomojica@gmail.com>
2022-01-29 20:27:53 +1100

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'template', loadChildren: () => import('./shared/template/template.module').then(m => m.TemplateModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
